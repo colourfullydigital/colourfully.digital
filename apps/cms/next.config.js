fbfbd9165c4bc9840ef9a7c1e0a4cff7dd01975c
delisma <david@colourfully.digital>
2025-05-22 10:25:30 -0400

    chore: remove Next.js files and ignore .astro build directory
    - Delete Next.js configuration and type files
    - Add .astro/ to .gitignore

--- apps/cms/next.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  transpilePackages: ["@colourfully-digital/shared"],
-  // Add any other Next.js configuration options here
-}
-
-module.exports = nextConfig
